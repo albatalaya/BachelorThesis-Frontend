a3308bdbb042d86e230bad2c8a48582a3377225b
Alba Talaya <62014163+albatalaya@users.noreply.github.com>
2022-01-18 23:56:38 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,19 @@
+import { CapacitorConfig } from '@capacitor/cli';
+import { Http2ServerRequest } from 'http2';
+
+const config: CapacitorConfig = {
+  appId: 'albatalaya.TFG',
+  appName: 'Optical warning of bicycle riders',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  /*server: {
+    //hostname: '192.168.0.33:5000',
+    androidScheme: 'https'
+    //url: 'https://192.168.0.33:5000'
+
+  }*/
+};
+
+
+export default config;
+
